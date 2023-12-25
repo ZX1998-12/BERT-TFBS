220e9b55f059a93983177a2b7f9430ef56b024e0
ZX <82807601+ZX1998-12@users.noreply.github.com>
2023-12-25 20:02:02 +0800

    Update pre_data.py

--- Dataset/wgEncodeAwgTfbsHaibHepg2Fosl2V0416101UniPk/pre_data.py
+++ Dataset/wgEncodeAwgTfbsHaibHepg2Fosl2V0416101UniPk/pre_data.py
@@ -2,12 +2,14 @@ import os
 import pandas as pd
 
 dataset_folder = 'dataset\\'
+# Iterate over 165 ENCODE ChIP-seq datasets.
 for folder_name in os.listdir(dataset_folder):
     folder_path = os.path.join(dataset_folder, folder_name)
     if os.path.isdir(folder_path):
         train_file = os.path.join(folder_path, 'train.data')
         test_file = os.path.join(folder_path, 'test.data')
         if os.path.isfile(train_file) and os.path.isfile(test_file):
+            # Convert the .data file to the .csv file.
             df = pd.read_csv(train_file, sep=' ', header=None, names=['chr_id', 'sequence', 'label'])
             train_csv_file = os.path.join(folder_path, f'train.csv')
             df.to_csv(train_csv_file, index=False)
